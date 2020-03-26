51725effde9bec34e7b47cae0bb08353e88d467e
sreejith <sreejith7874@gmail.com>
2020-03-27 00:02:33 +0530

    Exceptions handled

--- src/main/java/me/sreejithag/RateApiApplication.java
+++ src/main/java/me/sreejithag/RateApiApplication.java
@@ -2,6 +2,8 @@ package me.sreejithag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,31 +22,54 @@ public class RateApiApplication {
 		return "Rate_api Works";
 	}
 	@RequestMapping(value="/convertionrate")
-	public String getConvertionRate(@RequestParam(value = "from") String from, @RequestParam(value = "to") String to) {
+	public String getConvertionRate(@RequestParam(value = "from") String from, @RequestParam(value = "to") String to) throws IllegalAccessException {
 		
-		Services service = new Services();
-		double res = service.getConvertionRate(from, to);
-		if(res !=0) {
-			return String.valueOf(Math.round(res*100)/100.00);
+		if(null == from || null == to  ) {
+			throw new IllegalAccessException();
 		}
 		else {
-			return "Error Invalid currency code";
+			Services service = new Services();
+			double res = service.getConvertionRate(from, to);
+			if(res !=0) {
+				return String.valueOf(Math.round(res*100)/100.00);
+			}
+			else {
+				return "Error Invalid currency code";
+			}
 		}
 	}
 	
 	
 	@RequestMapping(value="/convert")
-	public String convert(@RequestParam(value = "amount") Double amount,@RequestParam(value = "from") String from, @RequestParam(value = "to") String to) {
+	public String convert(@RequestParam(value = "amount") Double amount,@RequestParam(value = "from") String from, @RequestParam(value = "to") String to) throws IllegalAccessException {
 		
-		Services service = new Services();
-		double res = service.convert(amount,from, to);
-		if(res !=0) {
-			return String.valueOf(Math.round(res*100)/100.00);
+		if( null == amount || null == from || null == to ) {
+			throw new IllegalAccessException();
 		}
+		
 		else {
-			return "Error Invalid currency code";
+			Services service = new Services();
+			double res = service.convert(amount,from, to);
+			if(res !=0) {
+				return String.valueOf(Math.round(res*100)/100.00);
+			}
+			else {
+				return "Error Invalid currency code";
+			}
 		}
 	}
+	
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public String handleMissingParams(MissingServletRequestParameterException ex) {
+	    String name = ex.getParameterName();
+	    return (name + " parameter is missing");
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String handleIlligalArgument(IllegalArgumentException ex) {
+		return "Parameter can't be empty";
+	}
+	
 	
 	
 }
