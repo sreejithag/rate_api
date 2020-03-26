24cb46a41e9f11d9d06db5d536701f2643d01e8d
sreejith <sreejith7874@gmail.com>
2020-03-26 22:03:08 +0530

    initial commit

---
+++ src/main/java/me/sreejithag/RateApiApplication.java
@@ -0,0 +1,36 @@
+package me.sreejithag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class RateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateApiApplication.class, args);
+	}
+	@RequestMapping(value="/")
+	public String api_response() {
+		return "Rate_api Works";
+	}
+	@RequestMapping(value="/convertionrate")
+	public String getConvertionRate(@RequestParam(value = "from") String from, @RequestParam(value = "to") String to) {
+		
+		Services service = new Services();
+		double res = service.getConvertionRate(from, to);
+		if(res !=0) {
+			return String.valueOf(res);
+		}
+		else {
+			return "Error Invalid currency code";
+		}
+	}
+	
+	
+}
---
+++ src/test/java/me/sreejithag/RateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sreejithag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
